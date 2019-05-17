baa72925c1f59807db57df5944a53082e4ddbe57
Vladimir Turcan <Vladimir.Turcan@endava.com>
2019-05-17 12:26:10 +0300

    made a small basic app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './product_manager.dart';
+
 void main() => runApp(App());
 
 class App extends StatelessWidget {
@@ -7,10 +9,10 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My App'),
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('My App'),
+          ),
+          body: ProductManager()),
     );
   }
 }
